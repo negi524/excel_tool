ab7f534e8fd876907dcb64d5db964039f01005ff
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-09-12 16:39:22 +0900

    インデックス名を定数化

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import pandas as pd
 LIST_FILE = "./resources/input/list.xlsx"
 TEMPLATE_FILE = "./resources/input/template.xlsx"
 
+KEY_1 = '施設名称'
+KEY_2 = '台数計算定員÷5'
+
 # エクセルファイルを読み込む
-df_list = pd.read_excel(LIST_FILE, sheet_name="Sheet1", header=8, index_col=1)
+df_list = pd.read_excel(LIST_FILE, header=8, index_col=1)
 
 # 必要な列のみ抽出する
-target_data = df_list[['施設名称', '台数計算定員÷5']]
+target_data = df_list[[KEY_1, KEY_2]]
 
 for index,row in target_data.iterrows():
     # 新規ファイル作成
@@ -16,9 +19,9 @@ for index,row in target_data.iterrows():
     # シートの1枚目を指定する
     sheet = book.worksheets[0]
     # C3のセルに施設名称を設定する
-    sheet['C4'] = row['施設名称']
-    sheet['D11'] = '： ' + row['施設名称']
-    sheet['G19'] = row['台数計算定員÷5']
+    sheet['C4'] = row[KEY_1]
+    sheet['D11'] = '： ' + row[KEY_1]
+    sheet['G19'] = row[KEY_2]
 
     # outputディレクトリ配下にtest_インデックス名.xlsxという名前で保存する
     book.save('./resources/output/' + str(index) + row['施設名称'] +  '.xlsx')
