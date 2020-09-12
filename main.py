d4328b05842c7dd372b26d5c9d5aeb3aebda2fc3
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-09-12 11:41:58 +0900

    エクセルファイルを開く

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('こんにちは')
+import openpyxl
+import pandas as pd
+
+LIST_FILE = "./resources/list.xlsx"
+
+# エクセルファイルを読み込む
+df_list = pd.read_excel(LIST_FILE, sheet_name="Sheet1", header=8, index_col=1)
+
+# 必要な列のみ抽出する
+data = df_list.loc[:,['施設名称', '台数計算定員÷5']]
+print(data)
