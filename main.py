9a7e69d391b6c3f341f8cc29d2992dc72ede5e08
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-09-12 16:27:31 +0900

    ファイル名を定数化

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import openpyxl
 import pandas as pd
 
 LIST_FILE = "./resources/input/list.xlsx"
+TEMPLATE_FILE = "./resources/input/template.xlsx"
 
 # エクセルファイルを読み込む
 df_list = pd.read_excel(LIST_FILE, sheet_name="Sheet1", header=8, index_col=1)
@@ -11,7 +12,7 @@ target_data = df_list[['施設名称', '台数計算定員÷5']]
 
 for index,row in target_data.iterrows():
     # 新規ファイル作成
-    book = openpyxl.load_workbook(filename='./resources/input/template.xlsx')
+    book = openpyxl.load_workbook(filename=TEMPLATE_FILE)
     # シートの1枚目を指定する
     sheet = book.worksheets[0]
     # C3のセルに施設名称を設定する
