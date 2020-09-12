a480df6af337fb306f4a0c0d83f58aeb6ffeb55a
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-09-12 14:13:51 +0900

    DataFrameとして抽出

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ LIST_FILE = "./resources/list.xlsx"
 df_list = pd.read_excel(LIST_FILE, sheet_name="Sheet1", header=8, index_col=1)
 
 # 必要な列のみ抽出する
-data = df_list.loc[:,['施設名称', '台数計算定員÷5']]
+data = df_list[['施設名称', '台数計算定員÷5']]
+
 print(data)
